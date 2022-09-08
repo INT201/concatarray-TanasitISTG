2df3e5a0cd6073e6daa27af56646cc2420f4439f
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-09-09 00:15:00 +0700

    test

--- concatArray.js
+++ concatArray.js
@@ -3,15 +3,19 @@ const { template } = require('@babel/core')
 function concatArray(array1, array2) {
   if ((array1 === null && array2 === null) || (array1 === undefined && array2 === undefined)) {
     return undefined;
+  } else if (array1 === undefined && array2 === null) {
+    return undefined;
+  } else if (array1 === null && array2 === undefined) {
+    return undefined;
   } else if (array1 === null || array1 === undefined) {
     return array2;
   } else if (array2 === null || array2 === undefined) {
     return array1;
-  } else if (array1 === undefined && array2 === null) {
-    return undefined;
   } else {
     return array1.concat(array2);
   }
 }
+
+console.log(concatArray(undefined, null));
 
 module.exports = concatArray
