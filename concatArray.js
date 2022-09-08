6ef0eaf62f528e61067f95567691510ccb78c5d6
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-09-09 00:02:34 +0700

    I'm questioning my self, what I've done 😂

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,43 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  if (array1 === null && array2 === null) {
+    return undefined
+  } else if (array1 === undefined && array2 === undefined) {
+    return undefined
+  } else if (array1 === [] && array2 === []) {
+    return []
+  } else if (array1 === null && array2 === undefined) {
+    return undefined
+  } else if (array1 === undefined && array2 === null) {
+    return undefined
+  } else if (array1 === null && array2 === []) {
+    return []
+  } else if (array1 === [] && array2 === null) {
+    return []
+  } else if (array1 === undefined && array2 === []) {
+    return []
+  } else if (array1 === [] && array2 === undefined) {
+    return []
+  } else if (array1 === null && array2 !== null && array2 !== undefined && array2 !== []) {
+    return array2
+  } else if (array1 !== null && array1 !== undefined && array1 !== [] && array2 === null) {
+    return array1
+  } else if (array1 === undefined && array2 !== null && array2 !== undefined && array2 !== []) {
+    return array2
+  } else if (array1 !== null && array1 !== undefined && array1 !== [] && array2 === undefined) {
+    return array1
+  } else if (array1 === [] && array2 !== null && array2 !== undefined && array2 !== []) {
+    return array2
+  } else if (array1 !== null && array1 !== undefined && array1 !== [] && array2 === []) {
+    return array1
+  } else if (array1 !== null && array1 !== undefined && array1 !== [] && array2 !== null && array2 !== undefined && array2 !== []) {
+    return array1.concat(array2)
+  } else {
+    return undefined
+  }
 }
+
+console.log(concatArray([ ], ['item1', 'item2', 'item3']))
+
 module.exports = concatArray
